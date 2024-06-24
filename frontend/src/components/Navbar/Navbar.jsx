b8df276ffdfb53921d995a67eecc2ea43d4b7beb
Jovan Simonovic <jovan.simonovic02@gmail.com>
2024-06-24 14:41:56 +0200

    Feature: Added search bar, Style: Changed logout button text to icon

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,22 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import ProfileInfo from "../Cards/ProfileInfo";
 import { useNavigate } from "react-router-dom";
+import SearchBar from "../SearchBar/SearchBar";
+import { FiLogOut } from "react-icons/fi";
 
 const Navbar = () => {
   const navigate = useNavigate();
+  const [searchQuery, setSearchQuery] = useState("");
 
   // handles logout request
-  const logout = async (e) => {
+  const logout = () => {
     navigate("/login");
+  };
+
+  // handles notes search request
+  const handleSearch = () => {
+    console.log(searchQuery);
   };
 
   return (
     <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
-      <h2 className="text-xl font-medium text-black py-2">Notes</h2>
+      <h2 className="text-xl font-medium text-black py-2 pr-2">Notes</h2>
+
+      <SearchBar
+        value={searchQuery}
+        onChange={(e) => {
+          setSearchQuery(e.target.value);
+        }}
+        handleSearch={handleSearch}
+      />
+
       <div className="flex gap-2">
         <ProfileInfo />
         <button className="btn-primary" onClick={logout}>
-          Log Out
+          <FiLogOut size={20} />
         </button>
       </div>
     </div>
--- frontend/src/components/SearchBar/SearchBar.jsx
+++ frontend/src/components/SearchBar/SearchBar.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { FaMagnifyingGlass } from "react-icons/fa6";
 
-const SearchBar = () => {
-  return <div>SearchBar</div>;
+const SearchBar = ({ value, onChange, handleSearch }) => {
+  return (
+    <div className="w-80 flex items-center px-4 bg-slate-100 rounded-md">
+      <input
+        type="text"
+        placeholder="Search notes"
+        className="w-full bg-transparent py-2 outline-none"
+        value={value}
+        onChange={onChange}
+      />
+      <FaMagnifyingGlass
+        className="text-slate-400 cursor-pointer hover:text-black"
+        onClick={handleSearch}
+      />
+    </div>
+  );
 };
 
 export default SearchBar;
