256107adadfcfa96bfdf17ddbdc14ec73f2b5fc8
Jovan Simonovic <jovan.simonovic02@gmail.com>
2024-07-07 23:40:03 +0200

    Style: Decreased gap between user name and profile icon, added left margin to ProfileInfo component

--- frontend/src/components/Cards/ProfileInfo.jsx
+++ frontend/src/components/Cards/ProfileInfo.jsx
@@ -4,7 +4,7 @@ import { CgProfile } from "react-icons/cg";
 const ProfileInfo = ({ user }) => {
   return (
     <>
-      <div className="flex items-center gap-2 cursor-pointer p-2 rounded-full hover:bg-slate-100">
+      <div className="flex items-center gap-1 cursor-pointer p-2 ml-2 rounded-full hover:bg-slate-100">
         <p>{user.username}</p>
         <CgProfile size={26} className="text-primary" />
       </div>
