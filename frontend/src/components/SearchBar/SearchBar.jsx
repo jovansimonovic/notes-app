01fcb9f555360f0e20867fb20b1015d41dd1ecc7
Jovan Simonovic <jovan.simonovic02@gmail.com>
2024-07-19 14:35:43 +0200

    Feature: Search now works when Enter is pressed

--- frontend/src/components/SearchBar/SearchBar.jsx
+++ frontend/src/components/SearchBar/SearchBar.jsx
@@ -2,6 +2,13 @@ import React from "react";
 import { FaMagnifyingGlass } from "react-icons/fa6";
 
 const SearchBar = ({ value, onChange, handleSearch }) => {
+  // checks if pressed button is Enter
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      handleSearch();
+    }
+  };
+
   return (
     <div className="w-80 flex items-center px-4 bg-slate-100 rounded-md">
       <input
@@ -10,6 +17,7 @@ const SearchBar = ({ value, onChange, handleSearch }) => {
         className="w-full bg-transparent py-2 outline-none"
         value={value}
         onChange={onChange}
+        onKeyDown={handleKeyDown}
       />
       <FaMagnifyingGlass
         className="text-slate-400 cursor-pointer hover:text-black"
