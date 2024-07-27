f06a7b680ec96847ed36a1924c20c87e9dedbb5a
Jovan Simonovic <jovan.simonovic02@gmail.com>
2024-07-27 12:00:36 +0200

    Feature: Added routes for Profile, Forgot Password and Reset Password

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,9 @@ import Home from "./pages/Home/Home";
 import LogIn from "./pages/LogIn/LogIn";
 import SignUp from "./pages/SignUp/SignUp";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ForgotPassword from "./pages/ForgotPassword/ForgotPassword";
+import ResetPassword from "./pages/ResetPassword/ResetPassword";
+import Profile from "./pages/Profile/Profile";
 
 const routes = (
   <Router>
@@ -10,6 +13,9 @@ const routes = (
       <Route path="/" exact element={<Home />} />
       <Route path="/login" exact element={<LogIn />} />
       <Route path="/signup" exact element={<SignUp />} />
+      <Route path="/forgotPassword" exact element={<ForgotPassword />} />
+      <Route path="/resetPassword" exact element={<ResetPassword />} />
+      <Route path="/profile" exact element={<Profile />} />
     </Routes>
   </Router>
 );
