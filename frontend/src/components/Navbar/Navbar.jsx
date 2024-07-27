a1fb503432573b792a2a1be2e873428af032cf68
Jovan Simonovic <jovan.simonovic02@gmail.com>
2024-07-27 12:02:53 +0200

    Refactor: Addded Link to profile component, Added Log In button

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import ProfileButton from "../Buttons/ProfileButton";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import SearchBar from "../SearchBar/SearchBar";
 import { FiLogOut } from "react-icons/fi";
 
@@ -23,6 +23,11 @@ const Navbar = ({ user, getAllNotes, searchNotes }) => {
     <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
       <h2 className="text-xl font-medium text-black py-2 pr-2">Notes</h2>
 
+      {location.pathname === "/forgotPassword" && (
+        <button className="btn-primary w-16">
+          <Link to="/login">Log In</Link>
+        </button>
+      )}
       {user && (
         <>
           <SearchBar
@@ -34,7 +39,9 @@ const Navbar = ({ user, getAllNotes, searchNotes }) => {
           />
 
           <div className="flex gap-2">
-            <ProfileButton user={user} />
+            <Link to="/profile">
+              <ProfileButton user={user} />
+            </Link>
             <button className="btn-primary" onClick={logout}>
               <FiLogOut size={20} />
             </button>
