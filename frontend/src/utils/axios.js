96035838fef1381f6dbfe3d723e75646bb541c98
Jovan Simonovic <jovan.simonovic02@gmail.com>
2024-10-31 01:04:29 +0100

    feature: added vercel.json, moved port to env file

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // creates axios instance with custom configuration
 const Axios = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: `http://localhost:${import.meta.env.VITE_PORT}`,
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
